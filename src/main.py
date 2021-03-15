fd6e9818caa8230cda7f578a9744c38182577b52
heitor57 <heitorwerneck@hotmail.com>
2021-03-15 13:24:17 -0300

    Batch size and invalid user insert

--- src/main.py
+++ src/main.py
@@ -4,15 +4,15 @@ import time
 from pymongo import MongoClient
 import utils
 
-max_num_iterations = 95000
+max_num_iterations = 90000
+batch_size = 10000
 
 api = utils.create_api()
 
 db = utils.start_db()
 
-
 if db.users.estimated_document_count()>0:
-    evaluated_users = db.users.find({}, {'steamid':1, 'friendslist':1 ,'_id':0})
+    evaluated_users = db.users.find({}, {'steamid':1, 'friendslist':1 ,'_id':0}).batch_size(batch_size)
     evaluated_users_id = set([i['steamid'] for i in evaluated_users])
     print(len(evaluated_users_id))
 
@@ -20,7 +20,8 @@ if db.users.estimated_document_count()>0:
     evaluated_users.rewind()
     for i in tqdm.tqdm(evaluated_users):
         # users_to_evaluate_id.union([j['steamid'] for j in i['friendslist']['friends']])
-        users_to_evaluate_id = users_to_evaluate_id | set([j['steamid'] for j in i['friendslist']['friends']])
+        users_to_evaluate_id = users_to_evaluate_id | {j['steamid'] for j in i['friendslist']['friends']}
+
     print(len(users_to_evaluate_id))
     users_to_evaluate_id = list(users_to_evaluate_id - evaluated_users_id)
 
@@ -34,14 +35,12 @@ else:
     users_to_evaluate_id=start_users_ids
     evaluated_users_id = set()
 
-
 num_iterations = 0
 
-# users_data = {}
 while len(users_to_evaluate_id) > 0 and num_iterations < max_num_iterations:
     try:
-        # user_to_evaluate_id = users_to_evaluate_id.pop(np.random.randint(0,len(users_to_evaluate_id)))
-        user_to_evaluate_id = users_to_evaluate_id.pop(0)
+        user_to_evaluate_id = users_to_evaluate_id.pop(np.random.randint(0,len(users_to_evaluate_id)))
+        # user_to_evaluate_id = users_to_evaluate_id.pop(0)
         num_iterations += 1
         print(num_iterations,user_to_evaluate_id,len(users_to_evaluate_id),len(evaluated_users_id))
         response = api.call('ISteamUser.GetFriendList', relationship='friend',steamid=user_to_evaluate_id)
@@ -56,6 +55,6 @@ while len(users_to_evaluate_id) > 0 and num_iterations < max_num_iterations:
         evaluated_users_id.add(user_to_evaluate_id)
         users_to_evaluate_id.extend(list(set([i['steamid'] for i in response['friendslist']['friends']])-evaluated_users_id))
     except Exception as e:
-        print(e)
+        db['users'].insert_one({'steamid': user_to_evaluate_id,'invalid':True})
 
 
