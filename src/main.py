0dd36b4edc88da32fc59de6200ec4bd78dd44727
heitor57 <heitorwerneck@hotmail.com>
2021-03-15 15:56:11 -0300

    Duplicate key treatment

--- src/main.py
+++ src/main.py
@@ -1,11 +1,14 @@
 from steam import webapi
+import pymongo
 import numpy as np
+from concurrent.futures import ProcessPoolExecutor, wait, FIRST_COMPLETED
 import tqdm
 import time
 from pymongo import MongoClient
 import utils
 
-max_num_iterations = 90000
+num_ptasks = 4
+max_num_iterations = 30000
 batch_size = 10000
 construction_buffer_max_size = 10000
 
@@ -45,26 +48,56 @@ else:
 
 num_iterations = 0
 
+def get_user_data(user_id):
+    response = api.call('ISteamUser.GetFriendList', relationship='friend',steamid=user_id)
+    user_data = {}
+    user_data.update(response)
+    user_data['steamid'] = user_to_evaluate_id
+    return user_data
+
+    # print(response)
+    # start_users_ids.append(response['response']['steamid'])
+
+    # users_to_evaluate_id.extend(list(set([i['steamid'] for i in user_data['friendslist']['friends']])-evaluated_users_id))
+
+
+# with ProcessPoolExecutor(num_ptasks) as executor:
+
+    # futures = set()
+    # while len(users_to_evaluate_id) > 0 and num_iterations < max_num_iterations:
+        # user_to_evaluate_id = users_to_evaluate_id.pop(np.random.randint(0,len(users_to_evaluate_id)))
+        # num_iterations += 1
+        # evaluated_users_id.add(user_to_evaluate_id)
+        # f = executor.submit(get_user_data,user_to_evaluate_id)
+        # futures.add(f)
+        # if len(futures) >= num_ptasks:
+            # completed, futures = wait(futures, return_when=FIRST_COMPLETED)
+
+    # for f in futures:
+        # f.result()
+
+
 while len(users_to_evaluate_id) > 0 and num_iterations < max_num_iterations:
     try:
-        user_to_evaluate_id = users_to_evaluate_id.pop(np.random.randint(0,len(users_to_evaluate_id)))
-        # user_to_evaluate_id = users_to_evaluate_id.pop(0)
-        num_iterations += 1
-        evaluated_users_id.add(user_to_evaluate_id)
-        print(num_iterations,user_to_evaluate_id,len(users_to_evaluate_id),len(evaluated_users_id))
+        try:
+            user_to_evaluate_id = users_to_evaluate_id.pop(np.random.randint(0,len(users_to_evaluate_id)))
+            # user_to_evaluate_id = users_to_evaluate_id.pop(0)
+            num_iterations += 1
+            evaluated_users_id.add(user_to_evaluate_id)
+            print(num_iterations,user_to_evaluate_id,len(users_to_evaluate_id),len(evaluated_users_id))
 
-        response = api.call('ISteamUser.GetFriendList', relationship='friend',steamid=user_to_evaluate_id)
+            response = api.call('ISteamUser.GetFriendList', relationship='friend',steamid=user_to_evaluate_id)
 
-        user_data = {}
-        user_data.update(response)
-        user_data['steamid'] = user_to_evaluate_id
-        db['users'].insert_one(user_data)
-        # print(response)
-        # start_users_ids.append(response['response']['steamid'])
-        
-        users_to_evaluate_id.extend(list(set([i['steamid'] for i in response['friendslist']['friends']])-evaluated_users_id))
-    except Exception as e:
+            user_data = {}
+            user_data.update(response)
+            user_data['steamid'] = user_to_evaluate_id
+            db['users'].insert_one(user_data)
+            # print(response)
+            # start_users_ids.append(response['response']['steamid'])
+            
+            users_to_evaluate_id.extend(list(set([i['steamid'] for i in response['friendslist']['friends']])-evaluated_users_id))
+        except Exception as e:
+            print(e)
+            db['users'].insert_one({'steamid': user_to_evaluate_id,'invalid':True})
+    except pymongo.errors.DuplicateKeyError as e:
         print(e)
-        db['users'].insert_one({'steamid': user_to_evaluate_id,'invalid':True})
-
-
