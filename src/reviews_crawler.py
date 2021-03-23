70f35f44d4eaf471bf66a37d261741e74957ef7c
heitor57 <heitorwerneck@hotmail.com>
2021-03-23 00:57:59 -0300

    Reuse

--- src/reviews_crawler.py
+++ src/reviews_crawler.py
@@ -2,6 +2,7 @@ from pymongo import MongoClient
 import urllib
 import traceback
 import requests
+import time
 from steam import webapi
 import utils
 import argparse
@@ -20,30 +21,36 @@ app = db.apps.find_one({'name': args.app})
 appid = app['appid']
 print(app['name'])
 # appid = '1220140'
-s = f'https://store.steampowered.com/appreviews/{appid}?json=1&cursor=*&day_range=9223372036854775807'
-print(s)
-response = requests.get(url=s)
-response = response.json()
-for review in response['reviews']:
-    review['appid'] = appid
-try:
-    db.reviews.insert_many(response['reviews'])
-except:
-    traceback.print_exc()
+cursor = '*'
+# s = f'https://store.steampowered.com/appreviews/{appid}?json=1&cursor=*'
+# print(s)
+# response = requests.get(url=s)
+# response = response.json()
+# for review in response['reviews']:
+    # review['appid'] = appid
+# try:
+    # db.reviews.insert_many(response['reviews'])
+# except:
+    # traceback.print_exc()
+is_end = False
 
-while response['query_summary']['num_reviews'] == 20:
+while not is_end:
+    print(cursor)
+    time.sleep(5)
     try:
-        s = f'https://store.steampowered.com/appreviews/{appid}?json=1&cursor={urllib.parse.quote(response["cursor"])}&day_range=9223372036854775807'
+        s = f'https://store.steampowered.com/appreviews/{appid}?json=1&cursor={cursor}'
         print(s)
         response = requests.get(url=s)
         response = response.json()
+        cursor = urllib.parse.quote(response["cursor"])
+        is_end = not (response['query_summary']['num_reviews'] == 20)
         # print(response)
         for review in response['reviews']:
             review['appid'] = appid
-
         db.reviews.insert_many(response['reviews'])
-    except:
-        traceback.print_exc()
+    except Exception as e:
+        print("Error",e)
+        # traceback.print_exc()
 
 # api.call('appreviews')
 # input()
